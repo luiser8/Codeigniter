1c8684698d40835106dde75bfecffe2fbffe7ae3
Luis Rondon <leduardo.rondon@gmail.com>
2019-07-21 21:52:42 -0400

    21-07-2019 21:52

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -10,27 +10,28 @@ class Auth extends CI_Controller {
         $this->load->library('session');
         $this->load->model('User');
         $this->load->model('Level');
-
-		if($this->verify_admin_level()){
-            redirect(base_url('Dashboard'));
-        }
     }
 
-	private function verify_admin_level(){
-        if($this->session->userdata("islogin") == TRUE){
-
-        }
-        //return $this->session->userdata("islogin");
-    }
+	// private function verify_admin_level(){
+ //        if($this->session->userdata("islogin") === true){}
+ //    }
 
 	public function index()
 	{
-		$this->load->view('Auth/index');
+		if($this->session->userdata("islogin") === true){
+            redirect(base_url('Dashboard'));
+        }else{
+        	$this->load->view('Auth/index');
+        }
 	}
 
 	public function signin()
 	{
-		$this->load->view('Auth/signin', ['Levels'=> $levels = $this->Level->All()]);
+		if($this->session->userdata("islogin") === true){
+            redirect(base_url('Dashboard'));
+        }else{
+        	$this->load->view('Auth/signin', ['Levels'=> $levels = $this->Level->All()]);
+        }
 	}
 
 	public function create()
@@ -67,26 +68,30 @@ class Auth extends CI_Controller {
 
 	public function login()
 	{
-		if($_POST)
-		{
-			$usuario = $this->User->Check($_POST);
-			if($usuario){
-				$usuario_data = array(
-					'user'=>$usuario['user'],
-					'account'=>$usuario['account'],
-					'pass'=>$usuario['pass'],
-					'first_name'=>$usuario['first_name'],
-					'last_name'=>$usuario['last_name'],
-					'email'=>$usuario['email'],
-					'id_level'=>$usuario['id_level'],
-					'islogin' => TRUE
-				);
-				$this->session->set_userdata($usuario_data);
-				//$this->Auditoria->Add($auditoria = array('idusuario'=>$_SESSION['IdUsuario'], 'tabla'=> 'Usuarios', 'accion'=>'Iniciar sesion', 'ip'=>isset($_SERVER['HTTP_CLIENT_IP']) ? $_SERVER['REMOTE_ADDR'] : '127.0.0.1'));
-            	redirect(base_url('Dashboard'));
-			}else{
-				$this->load->view('Auth/index', ['Error'=>'Error! usuario o contraseña invalido. Vuelve a intentar']);
-			}				
+		if($this->session->userdata("islogin") === true){
+            redirect(base_url('Dashboard'));
+        }else{
+			if($_POST)
+			{
+				$usuario = $this->User->Check($_POST);
+				if($usuario){
+					$usuario_data = array(
+						'user'=>$usuario['user'],
+						'account'=>$usuario['account'],
+						'pass'=>$usuario['pass'],
+						'first_name'=>$usuario['first_name'],
+						'last_name'=>$usuario['last_name'],
+						'email'=>$usuario['email'],
+						'id_level'=>$usuario['id_level'],
+						'islogin' => TRUE
+					);
+					$this->session->set_userdata($usuario_data);
+					//$this->Auditoria->Add($auditoria = array('idusuario'=>$_SESSION['IdUsuario'], 'tabla'=> 'Usuarios', 'accion'=>'Iniciar sesion', 'ip'=>isset($_SERVER['HTTP_CLIENT_IP']) ? $_SERVER['REMOTE_ADDR'] : '127.0.0.1'));
+	            	redirect(base_url('Dashboard'));
+				}else{
+					$this->load->view('Auth/index', ['Error'=>'Error! usuario o contraseña invalido. Vuelve a intentar']);
+				}				
+	        }
         }
 	}
 
@@ -98,6 +103,6 @@ class Auth extends CI_Controller {
       $this->session->set_userdata($usuario_data);
       $this->session->sess_destroy();
       //$this->Auditoria->Add($auditoria = array('id_user'=>$_SESSION['user'], 'tabla'=> 'Users', 'accion'=>'Cerrar sesion', 'ip'=>isset($_SERVER['HTTP_CLIENT_IP']) ? $_SERVER['REMOTE_ADDR'] : '127.0.0.1'));
-      redirect(base_url('Auth'));
+      redirect(base_url('Auth'), 'refresh');
    }
 }
